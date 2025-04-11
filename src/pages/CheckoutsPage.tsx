2c9cd3b96452ac44c210834c10a08546f6926129
Julio Canizalez <juliocanizalez@outlook.com>
2025-04-11 11:40:41 -0600

    Add checkouts page

---
+++ src/pages/CheckoutsPage.tsx
@@ -0,0 +1,10 @@
+import { AllCheckouts } from '../components/checkouts/AllCheckouts';
+import { Layout } from '../components/layout/Layout';
+
+export function CheckoutsPage() {
+  return (
+    <Layout>
+      <AllCheckouts />
+    </Layout>
+  );
+}
