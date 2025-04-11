14df5d9e6f4343bd0e2b72ae4ff7900cea2adc55
Julio Canizalez <juliocanizalez@outlook.com>
2025-04-11 11:40:21 -0600

    Add books page

---
+++ src/pages/BooksPage.tsx
@@ -0,0 +1,10 @@
+import { BookList } from '../components/books/BookList';
+import { Layout } from '../components/layout/Layout';
+
+export function BooksPage() {
+  return (
+    <Layout>
+      <BookList />
+    </Layout>
+  );
+}
