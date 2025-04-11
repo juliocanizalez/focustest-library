54c3a4bbb174f00f48938b7b1fb4167ed55bfe7f
Julio Canizalez <juliocanizalez@outlook.com>
2025-04-11 11:41:05 -0600

    Add my-checkouts page

---
+++ src/pages/BookDetailPage.tsx
@@ -0,0 +1,10 @@
+import { BookDetail } from '../components/books/BookDetail';
+import { Layout } from '../components/layout/Layout';
+
+export function BookDetailPage() {
+  return (
+    <Layout>
+      <BookDetail />
+    </Layout>
+  );
+}
---
+++ src/pages/MyCheckoutsPage.tsx
@@ -0,0 +1,10 @@
+import { MyCheckouts } from '../components/checkouts/MyCheckouts';
+import { Layout } from '../components/layout/Layout';
+
+export function MyCheckoutsPage() {
+  return (
+    <Layout>
+      <MyCheckouts />
+    </Layout>
+  );
+}
